00eb26386cd7d093756241e743f49c2b8ec3007e
KukshaBird <samoilovartem1989@gmail.com>
2020-01-14 21:16:31 +0200

    list allowed_hosts

--- DNZ144/settings.py
+++ DNZ144/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # False if not in os.environ
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS'), ]
+ALLOWED_HOSTS = [host for host in env.list('ALLOWED_HOSTS')]
 
 
 # Application definition
